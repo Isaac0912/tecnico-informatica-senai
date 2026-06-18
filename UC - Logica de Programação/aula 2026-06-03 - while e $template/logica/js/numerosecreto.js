4b62f6b97d15abeb01238b723f9d5f50043ff2b8
Isaac Pompeo <isaac_sp_silva@estudante.sesisenai.org.br>
2026-06-17 21:00:48 -0300

    fix: pequenos erros corrigidos

--- UC - Logica de Programação/aula 2026-06-03 - while e $template/logica/js/numerosecreto.js
+++ UC - Logica de Programação/aula 2026-06-03 - while e $template/logica/js/numerosecreto.js
@@ -1,14 +1,24 @@
 const numeroSecreto = Math.floor(Math.random() * 100) + 1;
 let chute = 0;
 let tentativas = 0;
-while (chute !== numeroSecreto) {
-    chute = Number(prompt("Adivinhe o número de 1 até 100: "));
-    tentativas++;
+while (chute !== numeroSecreto && tentativas < 7) {
+    chute = Number(prompt(`Adivinhe o número de 1 até 100 até chegar 7 tentativas.
+        \ntentativa: ${tentativas}.`));
+    if (chute === numeroSecreto) {
+        alert(`Acertou em ${tentativas} tentativas.`);
+        console.log("Acertou em", tentativas, "tentativas.");
+        break;
+    }
     if (chute < numeroSecreto) {
         console.log("Número muito baixo.");
         alert("Número muito baixo.");
     } else if (chute > numeroSecreto) {
         console.log("Número muito alto.");
         alert("Número muito alto.");
+    tentativas++;
     }
-}console.log("Acertou em", tentativas, "tentativas.");
+}
+
+if (tentativas === 7) {
+    console.log(`Game Over!\nO número era ${numeroSecreto}`)
+}
