b9472906b982848667f86c6529afc0bb3299d9f4
Isaac Pompeo <isaac_sp_silva@estudante.sesisenai.org.br>
2026-06-17 22:01:03 -0300

    docs: exemplos de for

---
+++ UC - Logica de Programação/aula 2026-06-17 - for loop/js/laco_repetição_for_n1.js
@@ -0,0 +1,26 @@
+let i;
+for (i = 0; i <= 5; i++) {
+    console.log("Iteração", i);
+}
+console.log("Decremento");
+for (i = 5; i >= 0; i--) {
+    console.log("Iteração", i);
+}
+
+console.log("Número Par.")
+for (i = 0; i <= 50; i++) {
+    if (i % 2 === 0) {
+        console.log("Número Par", i);
+    }
+}
+console.log("Tabuada.");
+let num = 7; //Tabuada do 7
+for (i = 0; i <= 10; i++) {
+    console.log(`${num} x ${i} = ${num * i}`);
+}
+console.log("Todas as tabuadas");
+for (i = 1; i <= 10; i++) {
+    for (j = 0; j  <= 10; j++) {
+        console.log(`${i} x ${j} = ${i * j}`);
+    }
+}
