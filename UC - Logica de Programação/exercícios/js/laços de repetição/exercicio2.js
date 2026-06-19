fe8b89bd475d207445f1887291878badc0ca05f7
ISAAC SAMUEL POMPEO DA SILVA <isaacpompeodasilva@gmail.com>
2026-06-19 20:09:49 -0300

    feat: adicionar exercícios de laço de repetição

---
+++ UC - Logica de Programação/exercícios/js/laços de repetição/exercicio2.js
@@ -0,0 +1,6 @@
+// 2. **Contagem regressiva**
+//     - Faça programa que imprima na tela a contagem regressiva de 10 até 1, um abaixo do outro.
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+}
