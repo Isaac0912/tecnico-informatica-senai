640ab506f5c6cec80ab348e9ab4b819f28f14af3
ISAAC SAMUEL POMPEO DA SILVA <isaac_sp_silva@estudante.sesisenai.org.br>
2026-06-26 22:14:52 -0300

    feat: novos exemplos de for loop

--- UC - Logica de Programação/aula 2026-06-17 - for loop/js/laco_repetição_for_n1.js
+++ UC - Logica de Programação/aula 2026-06-17 - for loop/js/laco_repetição_for_n1.js
@@ -1,27 +1,46 @@
-//Algoritmo criado pelo professor para ensino
-let i;
-for (i = 0; i <= 5; i++) {
-    console.log("Iteração", i);
-}
-console.log("Decremento");
-for (i = 5; i >= 0; i--) {
-    console.log("Iteração", i);
-}
+// //Algoritmo criado pelo professor para ensino
 
-console.log("Número Par.")
-for (i = 0; i <= 50; i++) {
-    if (i % 2 === 0) {
-        console.log("Número Par", i);
-    }
+// let i;
+// for (i = 0; i <= 5; i++) {
+//     console.log("Iteração", i);
+// }
+// console.log("Decremento");
+// for (i = 5; i >= 0; i--) {
+//     console.log("Iteração", i);
+// }
+
+// console.log("Número Par.")
+// for (i = 0; i <= 50; i++) {
+//     if (i % 2 === 0) {
+//         console.log("Número Par", i);
+//     }
+// }
+// console.log("Tabuada.");
+// let num = 7; //Tabuada do 7
+// for (i = 0; i <= 10; i++) {
+//     console.log(`${num} x ${i} = ${num * i}`);
+// }
+// console.log("Todas as tabuadas");
+// for (i = 1; i <= 10; i++) {
+//     for (j = 0; j  <= 10; j++) {
+//         console.log(`${i} x ${j} = ${i * j}`);
+//     }
+// }
+
+// let quantidadeDeNumeros = Number(prompt("Digite quantidade de números"));
+// let soma = 0, media = 0, i;
+// for (i = 1; i <= quantidadeDeNumeros; i++) {
+//     let numero = Number(prompt("Digite um número"));
+//     soma = soma + numero; // soma += numero
+// }
+// media = soma / quantidadeDeNumeros;
+// console.log("Soma", soma);
+// console.log("Média", media);
+
+//Fatorial
+let resultadoFatorial = 1;
+let numero = Number(prompt("Digite um número para calcular fatorial"));
+for (let i = numero; i >= 1; i--) {
+    resultadoFatorial = resultadoFatorial * i;
 }
-console.log("Tabuada.");
-let num = 7; //Tabuada do 7
-for (i = 0; i <= 10; i++) {
-    console.log(`${num} x ${i} = ${num * i}`);
-}
-console.log("Todas as tabuadas");
-for (i = 1; i <= 10; i++) {
-    for (j = 0; j  <= 10; j++) {
-        console.log(`${i} x ${j} = ${i * j}`);
-    }
-}
+console.log("O resultado do fatorial de", numero, "é", resultadoFatorial);
