42f5d68db5652d40698a0b5f0acc13e348aa7d23
Isaac <isaacpompeodasilva@gmail.com>
2026-06-28 22:59:10 -0300

    refactor: organizar exercicios para laço de repetição específico

---
+++ UC - Logica de Programação/exercícios/js/laços de repetição/for/exercicio4.js
@@ -0,0 +1,17 @@
+// 4. **Loja de R$ 1,99 do seu Manoel Joaquim**
+//     - O Sr. Manoel Joaquim possui uma grande loja de artigos de R$ 1,99, com cerca de 10 caixas. 
+// Para agilizar o cálculo de quanto cada cliente deve pagar, ele desenvolveu uma tabela que contém o número de itens que o cliente comprou e ao lado o valor da conta. 
+// Desta forma a atendente do caixa precisa apenas contar quantos itens o cliente está levando e olhar na tabela de preços. 
+// Você foi contratado para desenvolver o programa que monta esta tabela de preços, que conterá os preços de 1 até 50 produtos, conforme o exemplo abaixo:
+//         - `Lojas Quase Dois - Tabela de preços
+//         1 - R$ 1.99
+//         2 - R$ 3.98
+//         ...
+//         50 - R$ 99.50`
+
+console.log("Tabela de preços");
+console.log("valor de artigo: R$1.99");
+for (i = 1; i <= 50; i++) {
+    let valorArtigo = 1.99
+    console.log(`${i} - ${(i * valorArtigo).toFixed(2)}`)
+}
